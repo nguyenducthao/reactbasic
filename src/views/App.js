5dc57807b69b2ce35f97139818041e81ec4fe463
unknown <ndthaodng@gmail.com>
2021-10-30 19:28:13 +0700

    4 components

--- src/views/App.js
+++ src/views/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.scss';
+import MyComponent from './Example/MyComponent';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        <MyComponent />
       </header>
     </div >
   );
