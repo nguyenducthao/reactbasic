5ac2afc0ae64da7c63bc8c7ddbf98aff4753ebe9
unknown <ndthaodng@gmail.com>
2021-11-02 19:16:38 +0700

    14. Su dung dieu kien trong ReactJS

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,20 +1,37 @@
 import React from "react";
 class ChildComponent extends React.Component {
+    state = {
+        showJobs: false
+    }
+    handleShowHide = () => {
+        this.setState({
+            showJobs: !this.state.showJobs
+        })
+    }
     render() {
         let { name, age, arrayJobs } = this.props;
+        let { showJobs } = this.state;
         return (
             <>
                 <div>Child component name: {name}, age: {age}</div>
-                <div className='arrayJobs'>
-                    {
-                        arrayJobs.map((item) => {
-                            return (
-                                <div key={item.id}>{item.id} - {item.title} - {item.salary}</div>
-                            )
-                        })
-                    }
-                </div>
+                {!showJobs ?
+                    <div><button onClick={() => this.handleShowHide()}>Show jobs list</button></div>
+                    :
+                    <>
+                        <div className='arrayJobs'>
+                            {
+                                arrayJobs.map((item) => {
+                                    return (
+                                        <div key={item.id}>{item.id} - {item.title} - {item.salary}</div>
+                                    )
+                                })
+                            }
+                        </div>
+                        <div><button onClick={() => this.handleShowHide()}>Hide jobs list</button></div>
+                    </>
+                }
             </>
+
         )
     }
 }
